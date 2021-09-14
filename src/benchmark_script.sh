1c25f7e4ffdc94f8fb5a56c3de3f14ae7491cde0
Pham Khanh <khanhzodoi@gmail.com>
2021-09-14 22:33:59 +0700

    Modified gitignore, added src/benchmark_script.sh and src/pre_script.sh

---
+++ src/benchmark_script.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+PLAIN='\033[0m'
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${RED}Error:${PLAIN} This script must be run as root!" && exit 1
+
+
+next() {
+	printf "%-70s\n" "-" | sed 's/\s/-/g'
+}
+
+speed_test() {
+	local speedtest=$(wget -4O /dev/null -T300 $1 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}')
+	local ipaddress=$(ping -c1 -4 -n `awk -F'/' '{print $3}' <<< $1` | awk -F '[()]' '{print $2;exit}')
+	local nodeName=$2
+	printf "${YELLOW}%-40s${GREEN}%-16s${RED}%-14s${PLAIN}\n" "${nodeName}" "${ipaddress}" "${speedtest}"
+}
+
+speed_test_v6() {
+	local speedtest=$(wget -6O /dev/null -T300 $1 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}')
+	local ipaddress=$(ping6 -c1 -n `awk -F'/' '{print $3}' <<< $1` | awk -F '[()]' '{print $2;exit}')
+	local nodeName=$2
+	printf "${YELLOW}%-40s${GREEN}%-16s${RED}%-14s${PLAIN}\n" "${nodeName}" "${ipaddress}" "${speedtest}"
+}
+
+speed() {
+	speed_test 'http://cachefly.cachefly.net/100mb.test' 'CacheFly'
+	speed_test 'https://lax-ca-us-ping.vultr.com/vultr.com.100MB.bin' 'Vultr, Los Angeles, CA'
+	speed_test 'https://wa-us-ping.vultr.com/vultr.com.100MB.bin' 'Vultr, Seattle, WA'
+	speed_test 'http://speedtest.tokyo2.linode.com/100MB-tokyo.bin' 'Linode, Tokyo, JP'
+	speed_test 'http://speedtest.singapore.linode.com/100MB-singapore.bin' 'Linode, Singapore, SG'
+	speed_test 'http://speedtest.hkg02.softlayer.com/downloads/test100.zip' 'Softlayer, HongKong, CN'
+	speed_test 'http://speedtest1.vtn.com.vn/speedtest/random4000x4000.jpg' 'VNPT, Ha Noi, VN'
+	speed_test 'http://speedtest5.vtn.com.vn/speedtest/random4000x4000.jpg' 'VNPT, Da Nang, VN'
+	speed_test 'http://speedtest3.vtn.com.vn/speedtest/random4000x4000.jpg' 'VNPT, Ho Chi Minh, VN'
+	speed_test 'http://speedtestkv1a.viettel.vn/speedtest/random4000x4000.jpg' 'Viettel Network, Ha Noi, VN'
+	speed_test 'http://speedtestkv2a.viettel.vn/speedtest/random4000x4000.jpg' 'Viettel Network, Da Nang, VN'
+	speed_test 'http://speedtestkv3a.viettel.vn/speedtest/random4000x4000.jpg' 'Viettel Network, Ho Chi Minh, VN'
+	speed_test 'http://speedtesthn.fpt.vn/speedtest/random4000x4000.jpg' 'FPT Telecom, Ha Noi, VN'
+	speed_test 'http://speedtest.fpt.vn/speedtest/random4000x4000.jpg' 'FPT Telecom, Ho Chi Minh, VN'
+}
+
+fio_test() {
+	# Khanh
+	if [ -e '/usr/bin/fio' ]; then
+		echo "Fio Test"
+		local tmp=$(mktemp)
+		fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=fio_test --bs=4k --numjobs=1 --iodepth=64 --size=256M --readwrite=randrw --rwmixread=75 --runtime=30 --time_based --output="$tmp"
+		
+		if [ $(fio -v | cut -d '.' -f 1) == "fio-2" ]; then
+			local iops_read=`grep "iops=" "$tmp" | grep read | awk -F[=,]+ '{print $6}'`
+			local iops_write=`grep "iops=" "$tmp" | grep write | awk -F[=,]+ '{print $6}'`
+			local bw_read=`grep "bw=" "$tmp" | grep read | awk -F[=,B]+ '{if(match($4, /[0-9]+K$/)) {printf("%.1f", int($4)/1024);} else if(match($4, /[0-9]+M$/)) {printf("%.1f", substr($4, 0, length($4)-1))} else {printf("%.1f", int($4)/1024/1024);}}'`"MB/s"
+			local bw_write=`grep "bw=" "$tmp" | grep write | awk -F[=,B]+ '{if(match($4, /[0-9]+K$/)) {printf("%.1f", int($4)/1024);} else if(match($4, /[0-9]+M$/)) {printf("%.1f", substr($4, 0, length($4)-1))} else {printf("%.1f", int($4)/1024/1024);}}'`"MB/s"
+			
+		elif [ $(fio -v | cut -d '.' -f 1) == "fio-3" ]; then
+			local iops_read=`grep "IOPS=" "$tmp" | grep read | awk -F[=,]+ '{print $2}'`
+			local iops_write=`grep "IOPS=" "$tmp" | grep write | awk -F[=,]+ '{print $2}'`
+			local bw_read=`grep "bw=" "$tmp" | grep READ | awk -F"[()]" '{print $2}'`
+			local bw_write=`grep "bw=" "$tmp" | grep WRITE | awk -F"[()]" '{print $2}'`
+		fi
+
+		echo "Read performance     : $bw_read"
+		echo "Read IOPS            : $iops_read"
+		echo "Write performance    : $bw_write"
+		echo "Write IOPS           : $iops_write"
+		
+		rm -f $tmp fio_test
+	else
+		echo "Fio is missing!!! Please install Fio before running test."
+	fi
+}
+
+passmark_cpu() {
+	# Khanh 
+}
+
+passmark_memory() {
+	# Tuong 
+}
+
+iozone_filesystem() {
+	# Son 
+}
+
+
+test() {
+
+	echo "Date                 : $date"
+	echo ""
+	echo "Disk Speed"
+	next
+	fio_test $cores
+	echo ""
+	echo "CPU Speed"
+	next
+	sysbench_cpu && next
+	echo ""
+	echo "Speedtest"
+	next
+	printf "%-40s%-16s%-14s\n" "Node Name" "IPv4 address" "Download Speed"
+	speed && next
+}
+
+
+clear
+tmp=$(mktemp)
+test | tee $tmp
+cat $tmp >> ~/benchmark.log
---
+++ src/pre_script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+passmark_link=https://www.passmark.com/downloads/pt_linux_x64.zip
+iozone_link=http://www.iozone.org/src/current/iozone-3-491.x86_64.rpm
+passmark_dir="/passmark_lib"
+log_file_name="prescript.log"
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+PLAIN='\033[0m'
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${RED}Error:${PLAIN} This script must be run as root!" && exit 1
+
+## Install wget, unzip, fio
+if [ ! -e '/usr/bin/wget' ] || [ ! -e '/usr/bin/fio' ] || [ ! -e '/usr/bin/unzip' ] || [ ! -e '/usr/bin/unzip' ]
+then
+    yum clean all > /dev/null 2>&1 && yum install -y epel-release > /dev/null 2>&1 && yum install -y wget fio unzip git > /dev/null 2>&1 || (  apt-get update > /dev/null 2>&1 && apt-get install -y wget fio unzip git > /dev/null 2>&1 )
+fi
+
+## Install passmark
+PWD=`pwd`
+wget -q $passmark_link -O pt_linux_x64.zip > /dev/null 2>&1
+unzip -d $PWD pt_linux_x64.zip > /dev/null 2>&1
+
+# Make some changes to lib
+mkdir -p $passmark_dir
+ls -l /lib64/libstdc++.so.6 > $passmark_dir/vagranlib.bk
+cd $passmark_dir && git clone https://github.com/DevopsRizwan/requiredlibbin.git > /dev/null 2>&1
+cp "$passmark_dir/requiredlibbin/libstdc++.so.6.0.20" /lib64/libstdc++.so.6.0.20
+chmod +x /lib64/libstdc++.so.6.0.20
+unlink /lib64/libstdc++.so.6
+ln -s  /lib64/libstdc++.so.6.0.20 /lib64/libstdc++.so.6
+
+cd - > /dev/null 2>&1
+echo "Created $passmark_dir folder to download lib for running passmark" >> $log_file_name
+echo "Backup old soft link of /lib64/libstdc++.so.6 at $passmark_dir/vagranlib.bk file." >> $log_file_name
+echo "===========================" >> $log_file_name
+
+cp pt_linux_x64 /usr/bin/pt_linux_x64
+rm -rf ./pt_linux_x64
+
+## Install iozone
+if [ ! -e '/opt/iozone/bin/iozone' ];
+then
+    wget $iozone_link -O iozone-3-491.x86_64.rpm > /dev/null 2>&1 &&  sudo rpm -Uvh ./iozone-3-491.x86_64.rpm > /dev/null 2>&1 || apt-get install iozone -y > /dev/null 2>&1
+    ln -s /opt/iozone/bin/iozone /usr/bin/iozone
+    rm -rf iozone-3-491.x86_64.rpm
+
+fi
+
+echo "Install Done."
