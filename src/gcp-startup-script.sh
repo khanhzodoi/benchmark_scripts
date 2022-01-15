2f2cfb440d52831bec9a1147126bcc45132a1d0c
MININT-EPVG1V8-local <MININT-EPVG1V8-local@MININT-EPVG1V8>
2022-01-15 10:02:43 +0700

    update startup script

--- src/gcp-startup-script.sh
+++ src/gcp-startup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /root/benchmark/src/pre_script.sh
-/root/benchmark/src/benchmark_script.sh .
+/root/benchmark/src/benchmark_script.sh /root/benchmark/
 # do something to send log to server
 
 poweroff
